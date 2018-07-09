3bf926a118ae2fd04df11b1dab42ab702fa26a7a
Jared Grant <jared.grant@canyonsdistrict.org>
2018-07-09 11:14:41 -0600

    changed port and app title

---
+++ webpack/app.js
@@ -0,0 +1,11 @@
+import React, { component } from 'react';
+
+export default class App extends component {
+    render() {
+        return (
+            <div>
+                <h1>Bottega Madlibs</h1>
+            </div>
+        );
+    }
+}
